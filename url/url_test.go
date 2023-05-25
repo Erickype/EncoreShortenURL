4422af376b64cfd613dc71586fe5aa080378f32f
Erickype <67160282+Erickype@users.noreply.github.com>
2023-05-25 18:46:22 -0500

    Test for shorten and retrieve URL

---
+++ url/url_test.go
@@ -0,0 +1,29 @@
+package url
+
+import (
+	"context"
+	"testing"
+)
+
+// TestShortenAndRetrieve - test that the shortened URL is stored and retrieved from database.
+func TestShortenAndRetrieve(t *testing.T) {
+	testURL := "https://erickype.dev"
+	sp := ShortenParams{URL: testURL}
+	resp, err := Shorten(context.Background(), &sp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wantURL := testURL
+	if resp.URL != wantURL {
+		t.Errorf("got %q, want %q", resp.URL, wantURL)
+	}
+
+	firstURL := resp
+	gotURL, err := Get(context.Background(), firstURL.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if *gotURL != *firstURL {
+		t.Errorf("got %v, want %v", *gotURL, *firstURL)
+	}
+}
